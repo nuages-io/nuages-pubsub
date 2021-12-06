613defa62f7ceca1945f0f3cf4dc0289f148fd99
Martin Massé <martin@nuages.org>
2021-12-06 11:21:10 -0500

    ok

--- build.sh
+++
@@ -1,1 +0,0 @@
-dotnet publish /Users/martin/nuages-io/nuages-pubsub/Nuages.PubSub.Samples.Lambda/Nuages.PubSub.Samples.Lambda.csproj --configuration Release --framework net6.0 --self-contained true /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
