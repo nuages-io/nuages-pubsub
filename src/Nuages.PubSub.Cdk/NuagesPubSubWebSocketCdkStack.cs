eb38c259edbb7240945758fb043c41dfb21642bf
Martin Massé <martin@nuages.org>
2022-02-16 20:03:26 -0500

    ok

--- src/Nuages.PubSub.Cdk/NuagesPubSubWebSocketCdkStack.cs
+++ src/Nuages.PubSub.Cdk/NuagesPubSubWebSocketCdkStack.cs
@@ -1,11 +1,12 @@
 using System.Diagnostics.CodeAnalysis;
 using Amazon.CDK;
 using Amazon.CDK.AWS.Apigatewayv2;
-using Amazon.CDK.AWS.DynamoDB;
 using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.Route53;
 using Constructs;
+
+// ReSharper disable InconsistentNaming
 
 // ReSharper disable VirtualMemberNeverOverridden.Global
 // ReSharper disable MemberCanBeProtected.Global
@@ -20,21 +21,21 @@ namespace Nuages.PubSub.Cdk;
 [ExcludeFromCodeCoverage]
 public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
 {
-    public string? Asset { get; set; } 
-        
+    public string? Asset { get; set; }
+
     private string? OnConnectHandler { get; set; }
     private string? OnDisconnectHandler { get; set; }
     private string? OnAuthrorizeHandler { get; set; }
     private string? SendHandler { get; set; }
     private string? EchoHandler { get; set; }
     private string? JoinHandler { get; set; }
-    private string? LeaveHandler { get; set;}
-    
+    private string? LeaveHandler { get; set; }
+
     public string ApiName { get; set; } = "NuagesPubSub";
-    
+
     public string RouteSelectionExpression { get; set; } = "$request.body.type";
 
-    public string[] IdentitySource { get; set; } = { "route.request.querystring.access_token" } ;
+    public string[] IdentitySource { get; set; } = { "route.request.querystring.access_token" };
     public string AuthorizerName { get; set; } = "Authorizer";
 
     public string OnConnectFunctionName { get; set; } = "OnConnectFunction";
@@ -44,89 +45,88 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
     public string EchoFunctionName { get; set; } = "EchoFunction";
     public string JoinFunctionName { get; set; } = "JoinFunction";
     public string LeaveFunctionName { get; set; } = "LeaveFunction";
-        
+
     public string EchoRouteKey { get; set; } = "echo";
     public string SendRouteKey { get; set; } = "send";
     public string JoinRouteKey { get; set; } = "join";
     public string LeaveRouteKey { get; set; } = "leave";
-        
+
     public string StageName { get; set; } = "prod";
-           
+
     public string NuagesPubSubRole { get; set; } = "Role";
-    
+
     public NuagesPubSubWebSocketCdkStack(Construct scope, string id, IStackProps? props = null) : base(scope, id, props)
     {
-        
     }
 
     public const string ContextDomainName = "WebSocket_Domain";
     public const string ContextCertificateArn = "WebSocket_CertificateArn";
-    
+
     public const string ContextDomainNameApi = "API_Domain";
     public const string ContextCertificateArnApi = "PubSub_API_CertificateArn";
     public const string ContextApiKeyApi = "API_ApiKey";
-    
+
     public const string ContextCreateDynamoDbStorage = "Data_CreateDynamoDbStorage";
     public const string ContextTableNamePrefix = "Data_TableNamePrefix";
-    
+
     public const string ContextStorage = "Data_Storage";
     public const string ContextConnectionString = "Data_ConnectionString";
     public const string ContextDatabaseName = "Data_DatabaseName";
     public const string ContextAudience = "Audience";
     public const string ContextIssuer = "Issuer";
     public const string ContextSecret = "Secret";
-    
+
     public string? TableNamePrefix { get; set; }
     public string? Issuer { get; set; }
     public string? Audience { get; set; }
     public string? Secret { get; set; }
-    
+
     public string? Storage { get; set; }
     public string? ConnectionString { get; set; }
     public string? DatabaseName { get; set; }
-    
-    public List<CfnRoute> Routes { get; set; } = new ();
+
+    public List<CfnRoute> Routes { get; set; } = new();
 
     public virtual string MakeId(string id)
     {
         return $"{StackName}-{id}";
     }
-    
+
     // ReSharper disable once UnusedMember.Global
-    public  virtual void CreateTemplate()
+    public virtual void CreateTemplate()
     {
         NormalizeHandlerName();
 
         TableNamePrefix = Node.TryGetContext(ContextTableNamePrefix) != null!
             ? Node.TryGetContext(ContextTableNamePrefix).ToString()
             : null;
-        
+
         Issuer = Node.TryGetContext(ContextIssuer) != null!
             ? Node.TryGetContext(ContextIssuer).ToString()
             : null;
-        
+
         Audience = Node.TryGetContext(ContextAudience) != null!
             ? Node.TryGetContext(ContextAudience).ToString()
             : null;
-        
+
         Secret = Node.TryGetContext(ContextSecret) != null!
             ? Node.TryGetContext(ContextSecret).ToString()
             : null;
-        
+
         Storage = Node.TryGetContext(ContextStorage) != null!
             ? Node.TryGetContext(ContextStorage).ToString()
             : null;
-        
+
         ConnectionString = Node.TryGetContext(ContextConnectionString) != null!
             ? Node.TryGetContext(ContextConnectionString).ToString()
             : null;
-        
+
         DatabaseName = Node.TryGetContext(ContextDatabaseName) != null!
             ? Node.TryGetContext(ContextDatabaseName).ToString()
             : null;
-        
+
         var role = CreateWebSocketRole();
-        
+
         var api = CreateWebSocketApi();
 
         var onAuthorizeFunction = CreateFunction(OnAuthorizeFunctionName, OnAuthrorizeHandler, role, api);
@@ -134,42 +134,42 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
 
         var onConnectFunction = CreateFunction(OnConnectFunctionName, OnConnectHandler, role, api);
         CreateConnectRoute(api, authorizer, onConnectFunction);
-        
+
         var onDisconnectFunction = CreateFunction(OnDisconnectFunctionName, OnDisconnectHandler, role, api);
         CreateRoute("Disconnect", "$disconnect", api, onDisconnectFunction);
-        
+
         var sendFunction = CreateFunction(SendToGroupFunctionName, SendHandler, role, api);
         CreateRoute("Send", SendRouteKey, api, sendFunction);
-        
+
         var echoFunction = CreateFunction(EchoFunctionName, EchoHandler, role, api);
         CreateRoute("Echo", EchoRouteKey, api, echoFunction);
-        
+
         var joinFunction = CreateFunction(JoinFunctionName, JoinHandler, role, api);
         CreateRoute("Join", JoinRouteKey, api, joinFunction);
-        
+
         var leaveFunction = CreateFunction(LeaveFunctionName, LeaveHandler, role, api);
         CreateRoute("Leave", LeaveRouteKey, api, leaveFunction);
 
         CreateAdditionalFunctionsAndRoutes(api);
-        
+
         var deployment = CreateDeployment(api);
 
         var stage = CreateStage(api, deployment);
 
-        var domainName = (string) Node.TryGetContext(ContextDomainName);
+        var domainName = (string)Node.TryGetContext(ContextDomainName);
 
         if (!string.IsNullOrEmpty(domainName))
         {
             Console.WriteLine($"Domain = {domainName}");
-        
-            var certficateArn = (string) Node.TryGetContext(ContextCertificateArn);
-        
+
+            var certficateArn = (string)Node.TryGetContext(ContextCertificateArn);
+
             Console.WriteLine($"ContextCertificateArn = {ContextCertificateArn}");
-            
+
             var apiGatewayDomainName = CreateApiGatewayDomainName(certficateArn, domainName);
             CreateS3RecordSet(domainName, apiGatewayDomainName);
             CreateApiMapping(apiGatewayDomainName, api, stage);
-        
+
             // ReSharper disable once UnusedVariable
             var output2 = new CfnOutput(this, "NuagesPubSubCustomURI", new CfnOutputProps
             {
@@ -177,9 +177,10 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
                 Description = "The Custom WSS Protocol URI to connect to"
             });
         }
-        
-        var createDynamodb = Storage == "DynamoDb" && Convert.ToBoolean(Node.TryGetContext(ContextCreateDynamoDbStorage));
-        
+
+        var createDynamodb =
+            Storage == "DynamoDb" && Convert.ToBoolean(Node.TryGetContext(ContextCreateDynamoDbStorage));
+
         Console.WriteLine("createDynamodb = " + createDynamodb);
         if (createDynamodb)
         {
@@ -187,9 +188,9 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
         }
 
         var webSocketUrl = $"wss://{api.Ref}.execute-api.{Aws.REGION}.amazonaws.com/{stage.Ref}";
-        
+
         CreateWebApi(webSocketUrl);
-        
+
         // ReSharper disable once UnusedVariable
         var output = new CfnOutput(this, "NuagesPubSubURI", new CfnOutputProps
         {
@@ -197,8 +198,6 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
             Description = "The WSS Protocol URI to connect to"
         });
     }
-
-    
 
     private void NormalizeHandlerName()
     {
@@ -231,7 +230,6 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
     // ReSharper disable once UnusedParameter.Global
     protected virtual void CreateAdditionalFunctionsAndRoutes(CfnApi api)
     {
-        
     }
 
     protected virtual CfnDeployment CreateDeployment(CfnApi api)
@@ -263,10 +261,10 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
     protected virtual void GrantPermissions(Function func)
     {
         var principal = new ServicePrincipal("apigateway.amazonaws.com");
-            
+
         func.GrantInvoke(principal);
     }
-    
+
     protected virtual CfnStage CreateStage(CfnApi api, CfnDeployment deployment)
     {
         return new CfnStage(this, "Stage", new CfnStageProps
@@ -284,15 +282,15 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
             ApiId = api.Ref,
             AuthorizationType = "CUSTOM",
             AuthorizerId = authorizer.Ref,
-            //OperationName = "ConnectRoute",
             RouteKey = "$connect",
             RouteResponseSelectionExpression = null,
-            Target =  Fn.Join("/",  new [] { "integrations",CreateIntegration("ConnectInteg", api.Ref, onConnectFunction).Ref})
+            Target = Fn.Join("/",
+                new[] { "integrations", CreateIntegration("ConnectInteg", api.Ref, onConnectFunction).Ref })
         });
 
         Routes.Add(route);
     }
-        
+
     protected virtual void CreateRoute(string name, string key, CfnApi api, Function func)
     {
         // ReSharper disable once UnusedVariable
@@ -300,10 +298,9 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
         {
             ApiId = api.Ref,
             AuthorizationType = "NONE",
-            //OperationName = name + "Route",
             RouteKey = key,
             RouteResponseSelectionExpression = null,
-            Target = Fn.Join("/",  new [] { "integrations", CreateIntegration(name + "Integ", api.Ref, func).Ref}) 
+            Target = Fn.Join("/", new[] { "integrations", CreateIntegration(name + "Integ", api.Ref, func).Ref })
         });
     }
 
@@ -317,11 +314,11 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
                 $"arn:aws:apigateway:{Aws.REGION}:lambda:path/2015-03-31/functions/{func.FunctionArn}/invocations"
         });
     }
-        
+
     protected virtual CfnAuthorizer CreateAuthorizer(CfnApi api, Function onAuthorizeFunction)
     {
         var name = MakeId(AuthorizerName);
-        
+
         var authorizer = new CfnAuthorizer(this, name,
             new CfnAuthorizerProps
             {
@@ -339,11 +336,11 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
     protected virtual CfnApi CreateWebSocketApi()
     {
         var name = MakeId(ApiName);
-        
+
         var api = new CfnApi(this, name, new CfnApiProps
         {
             ProtocolType = "WEBSOCKET",
-            Name = name ,
+            Name = name,
             RouteSelectionExpression = RouteSelectionExpression
         });
         return api;
@@ -353,25 +350,24 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
     {
         if (string.IsNullOrEmpty(handler))
             throw new Exception($"Handler for {name} must be set");
-        
+
         if (string.IsNullOrEmpty(Asset))
             throw new Exception("Asset  must be set");
-        
+
         var func = new Function(this, name, new FunctionProps
         {
             Code = Code.FromAsset(Asset),
             Handler = handler,
             Runtime = Runtime.DOTNET_CORE_3_1,
-            //FunctionName = MakeId(name),
             MemorySize = 512,
             Role = role,
             Timeout = Duration.Seconds(30),
             Environment = new Dictionary<string, string>
             {
-                {"Nuages__PubSub__Region", Aws.REGION},
-                {"Nuages__PubSub__Uri", $"wss://{api.Ref}.execute-api.{Aws.REGION}.amazonaws.com/{StageName}"},
-                {"Nuages__PubSub__TableNamePrefix", TableNamePrefix ?? "" },
-                {"Nuages__PubSub__StackName", StackName },
+                { "Nuages__PubSub__Region", Aws.REGION },
+                { "Nuages__PubSub__Uri", $"wss://{api.Ref}.execute-api.{Aws.REGION}.amazonaws.com/{StageName}" },
+                { "Nuages__PubSub__TableNamePrefix", TableNamePrefix ?? "" },
+                { "Nuages__PubSub__StackName", StackName },
                 { "Nuages__PubSub__Issuer", Issuer ?? "" },
                 { "Nuages__PubSub__Audience", Audience ?? "" },
                 { "Nuages__PubSub__Secret", Secret ?? "" },
@@ -381,14 +377,14 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
             },
             Tracing = Tracing.ACTIVE
         });
-        
+
 
         GrantPermissions(func);
-        
+
         return func;
     }
 
-    
+
     protected virtual Role CreateWebSocketRole()
     {
         var role = new Role(this, NuagesPubSubRole, new RoleProps
@@ -400,7 +396,7 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
         role.AddManagedPolicy(CreateExecuteApiConnectionRolePolicy());
         role.AddManagedPolicy(CreateDynamoDbRolePolicy());
         role.AddManagedPolicy(CreateSystemsManagerParametersRolePolicy());
-            
+
         return role;
     }
 
@@ -410,44 +406,53 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
         {
             Document = new PolicyDocument(new PolicyDocumentProps
             {
-                Statements = new []{ new PolicyStatement(new PolicyStatementProps
+                Statements = new[]
                 {
-                    Effect = Effect.ALLOW,
-                    Actions = new []{"dynamodb:*"},
-                    Resources = new []{"*"}
-                })}
+                    new PolicyStatement(new PolicyStatementProps
+                    {
+                        Effect = Effect.ALLOW,
+                        Actions = new[] { "dynamodb:*" },
+                        Resources = new[] { "*" }
+                    })
+                }
             })
         });
     }
 
     protected virtual ManagedPolicy CreateExecuteApiConnectionRolePolicy(string suffix = "")
     {
-        return new ManagedPolicy(this, MakeId("ExecuteApiConnectionRole"+suffix), new ManagedPolicyProps
+        return new ManagedPolicy(this, MakeId("ExecuteApiConnectionRole" + suffix), new ManagedPolicyProps
         {
             Document = new PolicyDocument(new PolicyDocumentProps
             {
-                Statements = new []{ new PolicyStatement(new PolicyStatementProps
+                Statements = new[]
                 {
-                    Effect = Effect.ALLOW,
-                    Actions = new []{"execute-api:ManageConnections"},
-                    Resources = new []{"arn:aws:execute-api:*:*:*/@connections/*"}
-                })}
+                    new PolicyStatement(new PolicyStatementProps
+                    {
+                        Effect = Effect.ALLOW,
+                        Actions = new[] { "execute-api:ManageConnections" },
+                        Resources = new[] { "arn:aws:execute-api:*:*:*/@connections/*" }
+                    })
+                }
             })
         });
     }
 
     protected virtual ManagedPolicy CreateSystemsManagerParametersRolePolicy(string suffix = "")
     {
-        return new ManagedPolicy(this, MakeId("SystemsManagerParametersRole"+ suffix), new ManagedPolicyProps
+        return new ManagedPolicy(this, MakeId("SystemsManagerParametersRole" + suffix), new ManagedPolicyProps
         {
             Document = new PolicyDocument(new PolicyDocumentProps
             {
-                Statements = new []{ new PolicyStatement(new PolicyStatementProps
+                Statements = new[]
                 {
-                    Effect = Effect.ALLOW,
-                    Actions = new []{"ssm:GetParametersByPath"},
-                    Resources = new []{"*"}
-                })}
+                    new PolicyStatement(new PolicyStatementProps
+                    {
+                        Effect = Effect.ALLOW,
+                        Actions = new[] { "ssm:GetParametersByPath" },
+                        Resources = new[] { "*" }
+                    })
+                }
             })
         });
     }
@@ -458,14 +463,20 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
         {
             Document = new PolicyDocument(new PolicyDocumentProps
             {
-                Statements = new []{ new PolicyStatement(new PolicyStatementProps
+                Statements = new[]
                 {
-                    Effect = Effect.ALLOW,
-                    Actions = new []{"logs:CreateLogGroup",
-                        "logs:CreateLogStream",
-                        "logs:PutLogEvents"},
-                    Resources = new []{"*"}
-                })}
+                    new PolicyStatement(new PolicyStatementProps
+                    {
+                        Effect = Effect.ALLOW,
+                        Actions = new[]
+                        {
+                            "logs:CreateLogGroup",
+                            "logs:CreateLogStream",
+                            "logs:PutLogEvents"
+                        },
+                        Resources = new[] { "*" }
+                    })
+                }
             })
         });
     }
@@ -477,14 +488,14 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
         if (tokens.Length != 3)
             return domainName;
 
-        var tok  = new List<string>(tokens);
+        var tok = new List<string>(tokens);
         var remove = tokens.Length - 2;
         tok.RemoveRange(0, remove);
 
-        return tok[0] + "." + tok[1];                                
+        return tok[0] + "." + tok[1];
     }
-        
-    protected virtual void CreateS3RecordSet( string domainName, CfnDomainName apiGatewayDomainName)
+
+    protected virtual void CreateS3RecordSet(string domainName, CfnDomainName apiGatewayDomainName)
     {
         var hostedZone = HostedZone.FromLookup(this, "Lookup", new HostedZoneProviderProps
         {
@@ -513,13 +524,14 @@ public partial class NuagesPubSubWebSocketCdkStack<T> : Stack
         var apiGatewayDomainName = new CfnDomainName(this, "NuagesDomainName", new CfnDomainNameProps
         {
             DomainName = domainName,
-            DomainNameConfigurations = new [] { new CfnDomainName.DomainNameConfigurationProperty
+            DomainNameConfigurations = new[]
             {
-                EndpointType = "REGIONAL",
-                CertificateArn = certficateArn
-                
-            }}
-            
+                new CfnDomainName.DomainNameConfigurationProperty
+                {
+                    EndpointType = "REGIONAL",
+                    CertificateArn = certficateArn
+                }
+            }
         });
         return apiGatewayDomainName;
     }
