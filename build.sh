aebcb0cab93b9d71d1699900c8602de7151627b0
Martin Massé <martin@nuages.org>
2022-02-28 20:25:10 -0500

    ok

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-dotnet publish ./src/Nuages.PubSub.WebSocket.API/Nuages.PubSub.WebSocket.API.csproj --configuration Release --framework net6.0 --self-contained false /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
-dotnet publish ./src/Nuages.PubSub.API/Nuages.PubSub.API.csproj --configuration Release --framework net6.0 --self-contained false /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
+dotnet publish ./src/Nuages.PubSub.WebSocket.API/Nuages.PubSub.WebSocket.API.csproj --configuration Release --framework net6.0 --no-self-contained /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
+dotnet publish ./src/Nuages.PubSub.API/Nuages.PubSub.API.csproj --configuration Release --framework net6.0 --no-self-contained /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
