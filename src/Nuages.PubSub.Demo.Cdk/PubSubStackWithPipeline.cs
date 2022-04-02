26dff5d7ece9b80b7f88c264d44caeeb203aab0b
Martin Massé <martin@nuages.org>
2022-04-01 20:46:43 -0400

    ok

--- src/Nuages.PubSub.Demo.Cdk/PubSubStackWithPipeline.cs
+++ src/Nuages.PubSub.Demo.Cdk/PubSubStackWithPipeline.cs
@@ -62,7 +62,7 @@ public class PubSubStackWithPipeline : Stack
                     new (new PolicyStatementProps
                     {
                         Effect = Effect.ALLOW,
-                        Actions = new[] {  "ec2:DescribeSecurityGroups", "ec2:DescribeVpcs" },
+                        Actions = new[] {  "ec2:*" },
                         Resources = new[] { "*" }
                     })
                 }
